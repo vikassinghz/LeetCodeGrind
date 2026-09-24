7a58124d202dfd602f588e29524c9236cb5c7c02
Vikas Singh Baghel <vs.baghel2002@gmail.com>
2026-09-24 16:17:00 +0530

    Time: 43 ms (33.64%) | Memory: 31.7 MB (7.25%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,5 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
+
+        # BRUTE FORCE APPROACH
+
+        # if len(nums) < 0:
+        #     return nums
+
+        # Max = float('-inf')
+        # for i in range(len(nums)):
+        #     c_sum = 0
+        #     for j in range(i, len(nums)):
+        #         c_sum += nums[j]
+        #         if c_sum > Max:
+        #             Max = c_sum
+        # return Max    
+
+        #OPTIMAL APPROACH (KADANE'S ALGO)        
+        
         C_Sum = nums[0]
         Maximum = nums[0]
 
